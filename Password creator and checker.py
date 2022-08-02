cf0f945d87c5816e71ddb5181399481663c307fa
DpMaassen <110409928+DpMaassen@users.noreply.github.com>
2022-08-02 13:29:00 +0200

    Update Password creator and checker.py

--- Password creator and checker.py
+++ Password creator and checker.py
@@ -1,6 +1,10 @@
-#while loop for a string, using password example
+#while loop for a string, using password example.
+
+# initializing variables 
 password = ""
 number_guess = 5
+
+# Running the password loop.
 
 while password != "safe" and number_guess > 0:
     print("enter password")
@@ -11,6 +15,8 @@ while password != "safe" and number_guess > 0:
     else:
         print("false")
         number_guess = number_guess - 1
+        
+# while out of the loop check if password input was correct (within) 5 tries.        
 if number_guess > 0:
     print("welcome")
 else:
